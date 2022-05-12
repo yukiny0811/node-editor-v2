f8906ec02c1e093ceeeed1232da69e450ec12662
yukiny0811 <yukiny0811@gmail.com>
2022-05-12 18:26:03 +0900

    Initial Commit

---
+++ MVCNodeEditor/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  MVCNodeEditor
+//
+//  Created by クワシマ・ユウキ on 2022/05/12.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
